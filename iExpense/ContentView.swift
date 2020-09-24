b1890bebf874f53d477ce058d13adfc6535de662
PRABAL4546 <prabaljitwalia4546@gmail.com>
2020-09-24 13:04:52 +0530

    first commit

---
+++ iExpense/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by PRABALJIT WALIA     on 23/09/20.
+//
+
+import SwiftUI
+
+struct ExpenseItem{
+    let name: String
+    let type:String
+    let amount:Int
+}
+//array of expenseItem objects
+class Expenses:ObservableObject{
+    @Published var items = [ExpenseItem]()
+}
+struct ContentView: View {
+    @ObservedObject var expenses = Expenses()
+    
+    var body: some View {
+        NavigationView{
+            List{
+                ForEach(expenses.items, id:\.name){item in
+                    Text(item.name)
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationBarTitle("iExpense")
+            .navigationBarItems(trailing:
+                                    Button(action:{
+                                        let expense = ExpenseItem(name:"Test",type:"Personal",amount:5)
+                                        self.expenses.items.append(expense)
+                                    }){
+                                        Image(systemName: "plus")
+                                    }
+            )
+            
+        }
+     
+    }
+    func removeItems(at offsets:IndexSet){
+        expenses.items.remove(atOffsets: offsets)
+        
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by PRABALJIT WALIA     on 23/09/20.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
